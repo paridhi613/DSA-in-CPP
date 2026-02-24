316498d4b273d041f8a7d169fe926afbef1112ed
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-25 00:19:53 +0530

    comment added

--- Practice_Questions/Prime_No.cpp
+++ Practice_Questions/Prime_No.cpp
@@ -7,8 +7,8 @@ int main()
     cout << "enter the value: ";
     cin >> n;
     int i;
-    for (int i = 2; i < n; i++)
-    { // i=2,3,4,.....n-1 // if remainder become zero n is not prime
+    for (int i = 2; i < n; i++)      // i=2,3,4,.....n-1 // if remainder become zero n is not prime
+    { 
         if (n % i == 0)
         {
             cout << " not a prime";
