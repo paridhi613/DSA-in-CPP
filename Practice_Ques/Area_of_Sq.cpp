fc8cd65027a3b2db60668aef7266171a6e810933
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-25 23:35:46 +0530

    new folder created for practice questions

---
+++ Practice_Ques/Area_of_Sq.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the side of square: ";
+    cin >> n;
+
+    int area = n * n;
+    cout << "the area of square is: " << area;
+}
