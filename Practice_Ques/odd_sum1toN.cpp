e8c2761464352347c915d1a28ded098c7b701dbb
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-26 19:41:10 +0530

    sum of odd numbers from 1 to n

---
+++ Practice_Ques/odd_sum1toN.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the value: ";
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        if (i % 2 == 0)
+        {
+            continue;
+        }
+        sum = sum + i;
+    }
+    cout << sum;
+}
