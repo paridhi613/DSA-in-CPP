3b571bfc221f1db4936af81075d167f63f2842c9
paridhi613 <kalyaneparidhi94@gmail.com>
2026-02-27 23:27:48 +0530

    sum to even numbers from 1 to n using while loop

---
+++ Practice_Ques/even_sum_while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the value: ";
+    cin >> n;
+
+    int sum = 0;
+    int i = 1;
+
+    while (i <= n)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+        i++;
+    }
+    cout << sum;
+}
