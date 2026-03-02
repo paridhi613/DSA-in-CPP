7114afc7bddf1785d8837bebbfbeb04074427ece
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-02 17:46:12 +0530

    added one more logic

--- Patterns Practice/02_Char.cpp
+++ Patterns Practice/02_Char.cpp
@@ -3,16 +3,40 @@ using namespace std;
 
 int main()
 {
-    char ch;
-    cout << "enter the value: ";
-    cin >> ch;
+    // char ch;
+    // cout << "enter the value: ";
+    // cin >> ch;
 
-    for (char i = 'A'; i <= ch; i++)
+    // for (char i = 'A'; i <= ch; i++)
+    // {
+    //     for (char j = 'A'; j <= ch; j++)      //BASIC LOGIC
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+    // extra af logic
+    int n;
+    cout << "enter the value: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
     {
-        for (char j = 'A'; j <= ch; j++)
+
+        char ch = 'A';        /// starting value is fixed'A' for every inner loop
+        for (int j = 0; j < n; j++)        
         {
-            cout << j << " ";
+            cout << ch << " ";     
+            ch++;             //1 value get added every time loop runs
         }
+
         cout << endl;
     }
 }
