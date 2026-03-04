aedc226e0766b9c746833f0f3d0c46137dce0b60
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-04 22:50:30 +0530

    finding smallest in array

---
+++ 03_Arrays/3_SmallestArr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[] = {3, 45, 67, -12, 56, -2};
+    int size = 6;
+
+    int smallest = INT16_MAX;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (nums[i] < smallest)
+        {
+            smallest = nums[i];
+        }
+    }
+
+    cout << "smallest = " << smallest << endl;
+}
