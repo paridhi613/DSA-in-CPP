8a4a57e41b3ca2b393e8221317c96e5a58a2f1a8
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-04 22:29:58 +0530

    input and output at user end

--- 03_Arrays/2_LoopInArr.cpp
+++ 03_Arrays/2_LoopInArr.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int main()
 {
-    int marks[] = {12, 13, 14, 15, 16};
-    int size = 5;
+    //     int marks[] = {12, 13, 14, 15, 16};
+    //     int size = 5;
 
-    for (int i = 0; i < size; i++)   //i ranges from 0 to size-1;
+    //     for (int i = 0; i < size; i++)   //i ranges from 0 to size-1;
+    //     {
+    //         cout << marks[i] << endl;   //prints all the values from index 0 to 4 in new line
+    //     }
+
+
+
+    // taking input and output manually
+    int size = 5;
+    int marks[size];
+
+    for (int i = 0; i < size; i++)
     {
-        cout << marks[i] << endl;   //prints all the values from index 0 to 4 in new line 
+        cin >> marks[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << marks[i] << endl;
     }
 }
