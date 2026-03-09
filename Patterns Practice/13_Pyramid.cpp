66a29543f4cd7df74cfd2fcf615f386fc879d2f4
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-09 15:51:23 +0530

    comments added

--- Patterns Practice/13_Pyramid.cpp
+++ Patterns Practice/13_Pyramid.cpp
@@ -21,17 +21,17 @@ int main()
     for (int i = 0; i < n; i++)
     {
 
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < n - i - 1; j++)     //for printing spaces first
         {
             cout << " ";
         }
 
-        for (int j = 1; j <= i + 1; j++)
+        for (int j = 1; j <= i + 1; j++)      //then printing the numbers from right
         {
             cout << j;
         }
 
-        for (int j = i; j >= 1; j--)
+        for (int j = i; j >= 1; j--)         //printing numbers in reverse order
         {
             cout << j;
         }
