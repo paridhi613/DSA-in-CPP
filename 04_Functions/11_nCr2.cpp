7fde7020c07660f99d888a8a9699580960951f13
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-12 20:44:26 +0530

    comments added

--- 04_Functions/11_nCr2.cpp
+++ 04_Functions/11_nCr2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//function to calculate factorial
 int factorial(int n)
 {
     int fact = 1;
@@ -12,15 +13,19 @@ int factorial(int n)
     return fact;
 }
 
+
+//function to find the binomial coefficient
 int nCr(int n, int r)
 {
     int fact_n = factorial(n);
     int fact_r = factorial(r);
     int fact_nmr = factorial(n - r);
 
-    return fact_n / (fact_r * fact_nmr);
+    return fact_n / (fact_r * fact_nmr);  //this function calls the factorial function
 }
 
+
+//calling the function
 int main()
 {
     cout << nCr(8, 2);
