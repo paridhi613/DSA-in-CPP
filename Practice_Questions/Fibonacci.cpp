d9976fade679522626a02fb168e50bc498a83d62
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-14 15:03:34 +0530

    printing the fibonacci series

---
+++ Practice_Questions/Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "enter the value : ";
+    cin >> n;
+
+    int a = 0, b = 1, next;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
