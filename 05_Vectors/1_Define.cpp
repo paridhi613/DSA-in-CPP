7ffed2dfaff733addcaf2b7abd9e2bafa91f7a59
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-16 13:15:41 +0530

    added the second type

--- 05_Vectors/1_Define.cpp
+++ 05_Vectors/1_Define.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec={1,2,3};
-    cout<<vec[2];
+    //type 1
+    vector<int> vec = {1, 2, 3};
+    cout << vec[2] << endl;
 
+
+    //type 2
+    vector<int> vec2(3, 7);   //defines the size and value of vector
+
+    for (int val : vec2)
+    {
+        cout << val << " ";   //prints all the values of vector
+    }
 }
