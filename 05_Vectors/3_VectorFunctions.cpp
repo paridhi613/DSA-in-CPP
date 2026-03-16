a5cc3e508290763ee0f203f5d818bcc31f19a2dd
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-16 13:51:29 +0530

    push_back function added

--- 05_Vectors/3_VectorFunctions.cpp
+++ 05_Vectors/3_VectorFunctions.cpp
@@ -8,10 +8,19 @@ int main()
     // size function
     vector<char> vec = {'a', 'b', 'c', 'd'};
     cout << "size = " << vec.size() << endl;
-    
+    cout << endl;
 
     // push_back function
     vector<int> vec2;
+    cout << "size= " << vec2.size() << endl;
     vec2.push_back(22);
-    cout << vec2[0] << endl;
+    vec2.push_back(23);
+    vec2.push_back(24);
+
+    for (int a : vec2)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+    cout << "size after pushing= " << vec2.size() << endl;
 }
