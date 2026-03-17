f11dfc8b7ab573f849b5684cf36ca7be305e12a4
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-17 20:04:50 +0530

    added

--- 05_Vectors/4_SizeCapacity.cpp
+++ 05_Vectors/4_SizeCapacity.cpp
@@ -12,6 +12,15 @@ int main()
 
     cout <<"size= "<< vec.size() << endl;            //size will be 3
     cout <<"capacity= "<< vec.capacity() << endl;    //capacity will be 4
+
+
+    vec.push_back(23);
+    vec.push_back(24);
+
+    cout<<endl;
+
+    cout << "size= " << vec.size() << endl;           // size will be 5
+    cout << "capacity= " << vec.capacity() << endl;   //capacity will be 8
 }
 
 //double sized array is formed everytime if there is no space left for pushing new element.
