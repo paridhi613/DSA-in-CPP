8e58ae453202f829d409b9a7da4e59bc2dba9ea7
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-19 00:06:01 +0530

    sum of max subarray

---
+++ Array Questions/2_MaxSubarraySum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// printing the sum of maxium sub arrays possible from the given array
+
+int main()
+{
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int maxSum = INT16_MIN;
+
+    for (int st = 0; st < n; st++)
+    {
+        int currSum = 0;
+        for (int end = st; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
+        }
+    }
+
+    cout << "Max subarray sum= " << maxSum;
+}
