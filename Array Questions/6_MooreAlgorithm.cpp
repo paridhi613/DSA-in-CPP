2dba7733241282953ec89641bf36ee54cf52e2bf
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-22 16:25:59 +0530

    extra points added

--- Array Questions/6_MooreAlgorithm.cpp
+++ Array Questions/6_MooreAlgorithm.cpp
@@ -2,27 +2,47 @@
 #include <vector>
 using namespace std;
 
-//Moore's Algorithm to find the majority element
+// Moore's Algorithm to find the majority element
 int main()
 {
-    vector<int> nums = {2, 2, 1, 1, 1, 2, 2, 1, 1};
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2, 1, 1}; //{1, 2, 3, 4};
     int n = nums.size();
-    int freq = 0, ans = 0;                               //intially freq=0 so is ans=0
+    int freq = 0, ans = 0; // intially freq=0 so is ans=0
 
     for (int i = 0; i < n; i++)
     {
-        if (freq == 0)                                   //if freq=0 //ans will store the 1st ele of array
+        if (freq == 0) // if freq=0 //ans will store the 1st ele of array
         {
-            ans = nums[i];                               
+            ans = nums[i];
         }
-        if (ans == nums[i])                              //if ans=ele //move frwd
+        if (ans == nums[i]) // if ans=ele //move frwd
         {
             freq++;
         }
-        else                                             //if the next ele is not same as previ //move back
+        else // if the next ele is not same as previ //move back
         {
             freq--;
         }
     }
-    cout << ans;                                         //finally print the majority ele
+
+    // if any of majority element is not present
+    int count = 0;
+    for (int val : nums)
+    {
+        if (val == ans)
+        {
+            count++;
+        }
+    }
+
+    if (count > n / 2)
+    {
+        cout << ans;
+    }
+    else
+    {
+        cout << -1;
+    }
+
+     // finally print the majority ele
 }
