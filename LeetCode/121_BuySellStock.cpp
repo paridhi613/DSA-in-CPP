826b34836f3612ab8e9b0989cf593da0d389563f
paridhi613 <kalyaneparidhi94@gmail.com>
2026-03-26 21:07:39 +0530

    added

--- LeetCode/121_BuySellStock.cpp
+++ LeetCode/121_BuySellStock.cpp
@@ -8,7 +8,7 @@ int main()
 {
     vector<int> prices = {7, 1, 5, 3, 6, 4};
     int n = prices.size();
-    int maxProfit = 0, bestBuy = prices[0];                    //1st ele will the bestbuy at the start
+    int maxProfit = 0, bestBuy = prices[0];                    //1st element will the bestbuy at the start
 
     for (int i = 1; i < n; i++)                                //will check from the 1st index till the last
     {
