39bcb9b2efc4d0ca9a24284c1dc34a2ba1a00e98
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-01 20:56:58 +0530

    added

--- 06_Pointers/1_MemAdd.cpp
+++ 06_Pointers/1_MemAdd.cpp
@@ -6,10 +6,12 @@ int main()
     int a = 10;
     cout << &a; //& shows the memory address
 
-    
     cout << endl;
 
-
-    int *ptr = &a;  //pointer defined which stores the address of a
+    int *ptr = &a; // pointer defined which stores the address of a
     cout << ptr;
+
+    cout << endl;
+
+    cout << &ptr;  //address of pointer is different from a
 }
