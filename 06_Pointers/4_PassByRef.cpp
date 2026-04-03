51068e96ae90d1138334360232e658cc354fa330
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-03 21:21:24 +0530

    added

---
+++ 06_Pointers/4_PassByRef.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void changeA(int *ptr)   //pass by reference
+{
+    *ptr = 20;      //main function m jake a ki value 10 se 20 ho jaegi
+}
+
+int main()
+{
+    int a = 10;
+    changeA(&a);        //changeA fxn mai a ka address stored hai 
+
+    cout << "inside main fxn: " << a << endl;    //a ki value 10 se 20 ho jaegi 
+}
