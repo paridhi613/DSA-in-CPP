e1cab4d3ffbfddd9fa8b4b7e56f51ee4f4989bcf
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-04 13:44:37 +0530

    added

--- 06_Pointers/6_ArrayPointer.cpp
+++ 06_Pointers/6_ArrayPointer.cpp
@@ -5,5 +5,17 @@ int main()
 {
     int arr[] = {1, 2, 3, 4};
 
-    cout << *arr;   //will return the value at 0 index 1  here
+    cout << *arr;                       // will return the value at 0 index 1  here
+
+
+
+
+    // comparing the pointers
+    int *ptr1;
+    int *ptr2 = ptr1;
+
+    cout << ptr2 << endl;
+    cout << ptr1 << endl;
+
+    cout << (ptr2 == ptr1) << endl;
 }
