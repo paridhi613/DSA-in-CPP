09afb9a355b12d5f96be0552987f36f4262ccd40
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-05 20:41:17 +0530

    added

--- LeetCode/33_RotatedSortedArray.cpp
+++ LeetCode/33_RotatedSortedArray.cpp
@@ -1,7 +1,51 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int main(){
-    
+int search(vector<int> arr, int tar)
+{
+    int n = arr.size();
+    int st = 0, end = n - 1;
+
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+
+        if (arr[mid] == tar)
+        {
+            cout << mid;
+        }
+
+        // left half is sorted
+        if (arr[st] <= arr[mid])
+        {
+            if (arr[st] <= tar && tar <= arr[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                st = mid + 1;
+            }
+        }
+        else
+        { // right half is sorted
+            if (arr[mid] <= tar && tar <= arr[end])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {3, 4, 5, 6, 7, 0, 1, 2};
+    int tar = 2;
+
+    search(arr, tar);
 }
