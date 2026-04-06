ab84dcd03551297dad59b1485abe14aaa3cd6ced
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-06 22:56:30 +0530

    added

--- LeetCode/852_MountainPeak.cpp
+++ LeetCode/852_MountainPeak.cpp
@@ -5,23 +5,23 @@ using namespace std;
 int mountainPeak(vector<int> a, int peak)
 {
     int n = a.size();
-    int st = 1, end = n - 2;
+    int st = 1, end = n - 2;      //the fist and last index cant be peak so st with 1 and end n-2
 
     while (st <= end)
     {
         int mid = st + (end - st) / 2;
 
-        if (a[mid - 1] < a[mid] && a[mid] > a[mid + 1])
+        if (a[mid - 1] < a[mid] && a[mid] > a[mid + 1])   //mid == peak
         {
             return mid;
         }
-        else if (a[mid - 1] < a[mid])
+        else if (a[mid - 1] < a[mid])    //mid lies in left, search right 
         {
             st = mid + 1;
         }
         else
         {
-            end = mid - 1;
+            end = mid - 1;         //mid lies in right, search left
         }
     }
 }
