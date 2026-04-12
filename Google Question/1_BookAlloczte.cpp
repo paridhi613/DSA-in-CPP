4464f20d3183f69099295f6c7fb0485b052fec76
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-12 23:19:11 +0530

    added

---
+++ Google Question/1_BookAlloczte.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int allocateBooks(vector<int> &arr, int n, int m){
+
+}
+
+int  main(){
+    vector<int> arr= {2,1,3,4};
+    int n=4 , m=2;
+
+    cout << allocateBooks(arr, n,m) << endl;
+    
+}
