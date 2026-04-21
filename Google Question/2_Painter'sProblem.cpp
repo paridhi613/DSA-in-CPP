20b69fd7c02cca93c298d1ebc3a7702b5e5274cc
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-21 20:45:30 +0530

    added

--- Google Question/2_Painter'sProblem.cpp
+++ Google Question/2_Painter'sProblem.cpp
@@ -14,8 +14,17 @@ int minTimeToPaint(vector<int> &arr, int n, int m){
     //binary search statrs here
     while(st<=end){
         int mid = st +(end - st)/2;
-        
+
+        if(isPossible(n , m , mid, arr))
+        {
+            ans= mid ;
+            end = mid -1;
+        }else{
+            st = mid +1;
+        }
     }
+
+    return ans;
 }
 
 int main(){
