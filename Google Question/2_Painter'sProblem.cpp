df3c4f6ace6fcd4c843ebc01218645f0ffaea351
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-21 20:43:13 +0530

    added

---
+++ Google Question/2_Painter'sProblem.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int minTimeToPaint(vector<int> &arr, int n, int m){
+    int sum =0, maxVal = INT16_MIN;
+    for (int i = 0; i<n; i++){
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    int st = maxVal, end = sum, ans = -1;
+
+    //binary search statrs here
+    while(st<=end){
+        int mid = st +(end - st)/2;
+        
+    }
+}
+
+int main(){
+    vector<int> arr= {40,30,10,20};
+    int n = 4, m =2;
+
+    cout<< minTimeToPaint(arr,n,m)<<endl;
+}
